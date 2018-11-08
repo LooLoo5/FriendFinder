94657ec6f1d60a8d4f8eb6b5ff1cd3a7b461d8b2
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-11-08 13:34:13 -0600

    created dropdown and buttons

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+module.exports = {
+    matchData : [
+    {
+        'name': 'Jean-Luc Picard',
+        'photo': '',
+        'scores': [5,4,3,2,1,1,2,3,4,5]
+    },
+    {
+        'name': 'William T. Riker',
+        'photo': '',
+        'scores': [2,1,2,1,4,3,4,5,4,3]
+    },
+    {
+        'name': 'Data',
+        'photo': '',
+        'scores': [3,3,3,3,3,3,3,3,3,3]
+    },
+    {
+        'name': 'Deanna Troi',
+        'photo': '',
+        'scores': [2,4,3,4,3,5,1,1,2,3]
+    },
+    {
+        'name': 'Geordi La Forge',
+        'photo': '',
+        'scores': [1,1,1,1,3,2,4,3,5,4]
+    },
+    {
+        'name': 'Beverly Crusher',
+        'photo': '',
+        'scores': [5,3,4,3,4,2,1,2,4,5]
+    },
+    {
+        'name': 'Worf',
+        'photo': '',
+        'scores': [2,4,5,2,3,3,3,1,3,5]
+    },
+    {
+        'name': 'Wesley Crusher',
+        'photo': '',
+        'scores': [2,4,5,3,1,2,2,3,3,5]
+    },
+    {
+        'name': 'Tasha Yar',
+        'photo': '',
+        'scores': [4,5,2,3,1,2,1,4,3,5]
+    },
+    {
+        'name': 'Q',
+        'photo': '',
+        'scores': [5,2,4,3,1,5,5,3,2,2]
+    },
+],
+
+newFriend: [],
+bestMatch: []
+}
