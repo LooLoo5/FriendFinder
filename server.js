5c3d2af594cdea129e500237e8f7549b425dea97
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-11-14 17:32:00 -0600

    heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
